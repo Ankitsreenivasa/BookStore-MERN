15ab045271e72ec8f4fbc6244828a5653f43acd1
Ankitsreenivasa <ankit.sreeni@gmail.com>
2024-02-19 22:31:44 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,31 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/book.models.js";
 import booksRoute from "./routes/books.routes.js";
+import cors from "cors";
 
 const app = express();
 
 //Middleware for parsing request body
 app.use(express.json());
 
+//Middleware for handeling CORS policy
+//Option 1: Allow all origins with default of cors(*)
+//app.use(cors());
+//Option 2: Allow custom origins
+app.use(
+  cors({
+    origin: "http://localhost:8000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-type"],
+  })
+);
+
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("Welcome to MERN");
 });
 
-app.use('/books',booksRoute)
-
+app.use("/books", booksRoute);
 
 mongoose
   .connect(mongoDBURL)
