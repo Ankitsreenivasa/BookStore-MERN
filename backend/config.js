39409079584f1b5a41ae8768bf595643ba74418e
Ankitsreenivasa <ankit.sreeni@gmail.com>
2024-02-19 21:15:46 +0530

    basic setup+backend

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = "8000";
+
+export const mongoDBURL = 'mongodb+srv://root:Safari27@book-store-mern.5ttrmme.mongodb.net/?retryWrites=true&w=majority'
---
+++ backend/index.js
@@ -0,0 +1,31 @@
+import express, { response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/book.models.js";
+import booksRoute from "./routes/books.routes.js";
+
+const app = express();
+
+//Middleware for parsing request body
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("Welcome to MERN");
+});
+
+app.use('/books',booksRoute)
+
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
