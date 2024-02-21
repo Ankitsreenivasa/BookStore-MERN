96d1f414131c22a4d4401d84bcfc21364add8e54
Ankitsreenivasa <ankit.sreeni@gmail.com>
2024-02-21 21:55:36 +0530

    frontend setup

--- backend/index.js
+++ backend/index.js
@@ -12,15 +12,15 @@ app.use(express.json());
 
 //Middleware for handeling CORS policy
 //Option 1: Allow all origins with default of cors(*)
-//app.use(cors());
+app.use(cors());
 //Option 2: Allow custom origins
-app.use(
-  cors({
-    origin: "http://localhost:8000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-type"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:8000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-type"],
+//   })
+// );
 
 app.get("/", (req, res) => {
   console.log(req);
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import { Link } from "react-router-dom";
+import { AiOutlineEdit } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import BooksTable from "../components/home/BooksTable";
+import BooksCard from "../components/home/BooksCard";
+
+const Home = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  //const [showType, setShowType] = useState("table");
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("http://localhost:8000/books")
+      .then((res) => {
+        setBooks(res.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+  return <div>Home</div>;
+};
+
+export default Home;
